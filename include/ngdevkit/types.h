8bc27ea36f7ed6fbb17e19746405d2196d8d419c
Damien Ciabrini <damien.ciabrini@gmail.com>
2026-02-10 21:09:27 +0100

    Add typedef for 8byte-long types

--- include/ngdevkit/types.h
+++ include/ngdevkit/types.h
@@ -1,6 +1,6 @@
 /*
  * Shortcut types for C development on Neo Geo
- * Copyright (c) 2015-20205 Damien Ciabrini
+ * Copyright (c) 2015-2026 Damien Ciabrini
  * This file is part of ngdevkit
  *
  * ngdevkit is free software: you can redistribute it and/or modify
@@ -18,18 +18,21 @@
  */
 
 #ifndef __NGDEVKIT_TYPES_H__
+
 #define __NGDEVKIT_TYPES_H__
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef unsigned char  u8;
-typedef signed char    s8;
-typedef unsigned short u16;
-typedef signed short   s16;
-typedef unsigned int   u32;
-typedef signed int     s32;
+typedef unsigned char      u8;
+typedef signed char        s8;
+typedef unsigned short     u16;
+typedef signed short       s16;
+typedef unsigned int       u32;
+typedef signed int         s32;
+typedef unsigned long long u64;
+typedef signed long long   s64;
 
 #ifdef __cplusplus
 }
